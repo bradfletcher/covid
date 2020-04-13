7ed6cbf531aeb5de9873e565c0924f0a20a7bf9c
Brad Fletcher <brad@udel.edu>
2020-04-12 20:22:55 -0400

    added program to plot csv file

---
+++ plot.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.style as mplstyle
+mplstyle.use('fast')
+
+f = open("stats.csv", "r" ) 
+
+series1 = [] 
+series2 = [] 
+series3 = [] 
+xaxis = [] 
+
+# mpl.rcParams['path.simplify'] = True 
+# mpl.rcParams['path.simplify_threshold'] = 1.0 
+count = 0
+for l in f:
+    if(count != 0): 
+        values = []
+        values = l.split(",") 
+        xaxis.append(int( values[0] ) / 24.0 )
+        series1.append(int(values[1])) 
+        series2.append(int(values[2])) 
+        series3.append(int(values[3])) 
+    count +=1 
+
+plt.plot(xaxis, series1, 'r',  markevery=5, label="infected" )
+plt.plot(xaxis, series2, 'g',  markevery=5, label="recovered" )
+plt.plot(xaxis, series3, 'black',  markevery=5, label="dead" )
+plt.ylabel("people")
+plt.xlabel("days")
+plt.title("COVID-19 Simulation" )
+plt.legend(loc='upper left') 
+plt.savefig('figure1.pdf') 
+plt.show()
